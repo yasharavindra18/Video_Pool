3b24f3ab8dc2b8cefc284ff4bbbc518d886bd765
gavindya <gavindya@cs.odu.edu>
2019-02-25 17:02:51 -0500

    Ionic Camera mocule - initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public base64Image: string;
+  constructor(private camera: Camera) { }
+
+  takePicture(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+    let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (error) => {
+    // Handle error
+    });
+  
+  }
+
+}
