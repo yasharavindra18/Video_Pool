1824785ea2241f97d4425c5b508fa854f61a09dd
hpendyal <hpend001@odu.edu>
2019-02-26 10:10:11 -0500

    added GPS permissions to the repo.

---
+++ CameraControl/app/src/main/java/com/example/cameracontrol/CameraPreview.java
@@ -0,0 +1,108 @@
+package com.example.cameracontrol;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Camera;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback{
+    private SurfaceHolder mHolder;
+    private android.hardware.Camera mCamera;
+
+    public CameraPreview(Context context, android.hardware.Camera camera) {
+        super(context);
+        mCamera = camera;
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            // create the surface and start camera preview
+            if (mCamera == null) {
+                //mCamera.setDisplayOrientation(180);
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+                //setCameraDisplayOrientation((Activity) getContext(),1,mCamera);
+
+            }
+        } catch (IOException e) {
+            Log.d(VIEW_LOG_TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void refreshCamera(android.hardware.Camera camera) {
+        if (mHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        // start preview with new settings
+        setCamera(camera);
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            Log.d(VIEW_LOG_TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+        refreshCamera(mCamera);
+    }
+
+    public void setCamera(android.hardware.Camera camera) {
+        //method to set a camera instance
+        mCamera = camera;
+        mCamera.setDisplayOrientation(90);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        // mCamera.release();
+
+    }
+
+    /*public static void setCameraDisplayOrientation(Activity activity,
+                                                   int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == android.hardware.Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
+    }*/
+}
