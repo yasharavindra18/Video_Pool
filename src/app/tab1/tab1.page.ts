9358d7859c4a509cfc6435dbe094f8d9004139a7
yravindra <yravindra@cs.odu.edu>
2019-02-25 23:42:24 -0500

    fixed camera issue

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
 import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview/ngx';
 
 @Component({
@@ -11,41 +13,57 @@ import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, Camer
 export class Tab1Page {
 
   public picture;
+  
 
-  constructor(private cameraPreview: CameraPreview) { 
-    this.pic();
-  }
+  constructor(private camera: Camera, private cameraPreview: CameraPreview) 
+  { 
+     this.pic();
+  } 
 
   pic() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  
     const cameraPreviewOpts: CameraPreviewOptions = {
       x: 0,
       y: 0,
       width: window.screen.width,
       height: window.screen.height,
-      camera: 'front',
+      camera: 'rear',
       tapPhoto: true,
       previewDrag: true,
       toBack: true,
       alpha: 1
     }
-
+  
+    
     // start camera
     this.cameraPreview.startCamera(cameraPreviewOpts).then(
       (res) => {
-        console.info(res)
+        console.log(res)
       },
       (err) => {
-        console.error(err)
+        console.log(err)
       });
-
-
-    // picture options
     const pictureOpts: CameraPreviewPictureOptions = {
       width: 1280,
       height: 1280,
       quality: 85
     }
-
+    
     // take a picture
     this.cameraPreview.takePicture(pictureOpts).then((imageData) => {
       this.picture = 'data:image/jpeg;base64,' + imageData;
@@ -53,14 +71,14 @@ export class Tab1Page {
       console.log(err);
       this.picture = 'assets/img/test.jpg';
     });
-
-
+    
+    
     // Switch camera
     this.cameraPreview.switchCamera();
-
+    
     // set color effect to negative
     this.cameraPreview.setColorEffect('negative');
-
+    
     // Stop the camera preview
     this.cameraPreview.stopCamera();
   }
