42ae7825306a1283652bdc9e1e17a5a996c109d7
Yasith Jayawardana <yasith.jayawardana@icloud.com>
2019-02-27 22:06:54 -0500

    add chunking

--- src/global/streaming.ts
+++ src/global/streaming.ts
@@ -1,15 +1,34 @@
+import axios, { AxiosInstance } from 'axios';
+
 export class StreamingService {
 
-  startStreaming(localStream: MediaStream) {
+  private axiosInstance: AxiosInstance = axios.create({
+    baseURL: "http://localhost:3000/api",
+    timeout: 10000
+  });
+
+  public startStreaming(localStream: MediaStream) {
     const mediaRecorder = new MediaRecorder(localStream);
     mediaRecorder.ondataavailable = (event: BlobEvent) => {
-      console.log(event);
+      console.log(event.data.size);
       // Stream the blob to server
     };
-    mediaRecorder.start(1000);
+    mediaRecorder.start(100);
   }
 
-  stopStreaming() {
+  private pushToServer(blob: Blob){
+      const data = new FormData();
+      data.append('file', blob);
+    
+      return this.axiosInstance.post(`/livestream`, data, {
+        headers: {
+          'Content-Type': `multipart/form-data; boundary=${blob.size}`,
+        },
+        timeout: 30000,
+      });
+  }
+
+  public stopStreaming() {
     // this.connection.close();
   }
 }
